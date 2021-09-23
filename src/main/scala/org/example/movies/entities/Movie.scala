1ba935af234576e0cec7b0415e03e801e01276f7
Amina Adewusi <amina.adewusi@guardian.co.uk>
2021-09-23 10:30:20 +0100

    adds completed project code, exercises with answers and README introduction

--- src/main/scala/org/example/movies/entities/Movie.scala
+++ src/main/scala/org/example/movies/entities/Movie.scala
@@ -25,5 +25,46 @@ object Movie {
 
   private val logger = LoggerFactory.getLogger(this.getClass)
 
-  def parse(row: Map[String, String]): Option[Movie] = ???
+  def parse(row: Map[String, String]): Option[Movie] = {
+
+    val movie = for {
+      /* MANDATORY FIELDS */
+      genres <- parseGenres(row, "genres")
+      id <- parseInt(row, "id")
+      imdbId <- parseString(row, "imdb_id")
+      originalLanguage <- parseString(row, "original_language")
+      originalTitle <- parseString(row, "original_title")
+      overview <- parseString(row, "overview")
+      budget <- parseInt(row, "budget")
+    } yield {
+      /* NULLABLE FIELDS */
+      val popularity = parseFloat(row, "popularity")
+      val releaseDate = parseLocalDate(row, "release_date")
+      val runtimeInMinutes = parseDouble(row, "runtime")
+
+      /* NULLABLE FIELDS WITH DEFAULTS */
+      val revenue = parseInt(row, "revenue").getOrElse[Int](0)
+      val title = parseString(row, "title").getOrElse(originalTitle)
+      val voteAverage = parseFloat(row, "vote_average").getOrElse[Float](0)
+      val voteCount = parseFloat(row, "vote_count").getOrElse[Float](0)
+
+      Movie(genres,
+        id,
+        imdbId,
+        originalLanguage,
+        originalTitle,
+        title,
+        overview,
+        popularity,
+        releaseDate,
+        revenue,
+        budget,
+        runtimeInMinutes,
+        voteAverage,
+        voteCount)
+    }
+
+    if (movie.isEmpty) logger.warn(s"Skipping malformed movie row")
+    movie
+  }
 }
